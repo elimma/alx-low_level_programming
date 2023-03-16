1eb4a4c09ecbd366e3ad9f40b106450ce60e9607
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-16 12:18:50 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * * main - Entry point
  * * Return: Always 1
  */
 int main(void)
 {
-	printf(stdout, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	fprintf(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }

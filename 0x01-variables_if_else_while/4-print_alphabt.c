a6cd8144a51ca7fd2277e00ec38ef13b4207b51d
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-17 09:17:25 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main()
+{
+	char ch;
+	
+	for (ch ='a' ; ch <= 'z'; ++ch)
+	{
+		if ((ch == 'q' || ch == 'e') ==0)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

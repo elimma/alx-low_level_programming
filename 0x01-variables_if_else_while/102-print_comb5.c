84dd6771dbc9dd72f89b3185d1a27bdbe79f1a30
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-17 12:59:06 -0700

    combin 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if ( i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

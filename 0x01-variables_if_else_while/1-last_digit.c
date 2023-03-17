d8b8810c7c6773d12fb15efc9b6521c1529d2471
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-17 06:58:08 -0700

    positive negative

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determine if a random number  is greather, less or 0
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+        int n;
+        
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n % 10 != 0 && n % 10 < 6)
+        {
+          printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+        }
+        else if (n % 10 >5)
+        {
+          printf("Last digit of %d is %d and is greather tan 5\n", n, n % 10);
+        }
+        else 
+        {
+          printf("Last digit of %d is %d and is 0\n", n, n % 10);
+        }
+        return (0);
+}

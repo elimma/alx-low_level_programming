75a2c59955a993062e6af2ae4d4ce8b7d86da1d8
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-17 08:13:12 -0700

    lower alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-e
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * main - prints all the alphabets in lowercase 
+ *
+ * Return 0 on success
+ */
+int main()
+{
+	char ch;
+	for (ch ='a';ch <= 'z'; ++ch)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}

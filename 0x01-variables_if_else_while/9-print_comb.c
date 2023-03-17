611bcc60741ee37d9131128728562a537dadff61
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-17 11:54:38 -0700

    combinaison

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i < 9)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+	return 0;
+}

7655b75718fdfd6f5fc9427eb15f43dbc2bcd874
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-21 16:23:50 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		int i;
 
-		for ( i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	{
 		int i;
 
-		for ( i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)

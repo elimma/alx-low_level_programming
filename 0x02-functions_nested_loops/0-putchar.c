e1da85ba21954cf4f3036996e7f35b1daf79dc9d
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-21 08:00:06 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,13 @@
-#include <main.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * 
  * Return: 0 on success
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int main(void)
 {
 	char c[] = "_putchar";

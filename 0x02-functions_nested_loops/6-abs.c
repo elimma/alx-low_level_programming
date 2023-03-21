adbedc596c6f955300c9805ab98bc4615cbbb50b
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-21 13:59:50 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the ansolute value of an integer
+ *
+ * Return: 0 on success
+ */
+int _abs(int number)
+{
+	if (number < 0)
+	{
+		return (-number);
+	}
+	else
+	{
+		return (number);
+	}
+}

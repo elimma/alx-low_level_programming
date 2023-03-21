9b99e9116803066fafd5a20db0304693a5bde7bd
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-21 09:49:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 on succuess
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+}

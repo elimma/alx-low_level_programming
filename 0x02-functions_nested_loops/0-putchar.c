3aea52f2f74a53b82b1bbb2c96d46044d3bfaa45
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-21 07:23:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,12 @@
  */
 int main(void)
 {
-	char *cr = "_putchar";
-	putchar(*cr);
+	char c[] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		putchar(c[i]);
+	}
 	putchar('\n');
 	return (0);
 }

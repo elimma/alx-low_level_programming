f3499d4bd6e52b5aa84a68117146c3eb86e47214
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-23 16:42:59 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *
+ * print_diagonal- draws diagonal line
+ *
+ * @n: input
+ *
+ * Return: 0  on success
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i,j;
+
+		for (i =0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}

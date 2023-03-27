492cf69864cf7a04771f3d2c7a59fb706d8c0f18
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-27 13:03:32 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_ string - reverse string
+ *
+ * @s: input
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int ln = strlen(s);
+	int i;
+
+	for (i = 0; i < ln / 2; i++)
+	{
+		char t = s[i];
+		s[i] = s[ln - i - 1];
+		s[ln - i - 1] = t;
+	}
+}

4faf3f828af249bfada08fb0b9f1812de6706ddf
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-29 16:41:42 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - capitalizes all words
+ * @sttr: input
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int cap_words = 1;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		if (cap_words && isalpha(*p))
+		{
+			*p = toupper(*p);
+			cap_words = 0;
+		}
+		else if (isspace(*p) || ispunct(*p))
+		{
+			cap_words = 1;
+		}
+		p++;
+	}
+	return (str);
+}

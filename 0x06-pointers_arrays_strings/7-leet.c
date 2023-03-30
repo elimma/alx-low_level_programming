7232068814f305d752fe297b0fa737a5a9142e7e
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-29 17:37:56 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *leet - encodes a string
+ *
+ * @str: input 
+ * Return: result
+ */
+char *leet(char *str)
+{
+	char *result= str;
+	char *letters = "aeotlAEOTL";
+	char *replace = "4307143071";
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				result[i] = replace[j];
+				break;
+			}
+		}
+	}
+	return (result);
+}

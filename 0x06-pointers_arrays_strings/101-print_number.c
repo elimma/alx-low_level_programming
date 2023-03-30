7a7f5b3266fcba5f2023a80bd02479b4fe54dddc
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-03-30 12:29:54 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,17 @@
  */
 void print_number(int n)
 {
+	unsigned int b;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		 n *= -1;
 	}
-	if (n / 10 != 0)
+	b = n;
+	if (b / 10)
 	{
-		print_number(n / 10);
+		print_number(b / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(b % 10 + '0');
 }

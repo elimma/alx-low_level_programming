51fc32232afd5dd701e71298a4739a7b7b2063da
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-04-11 15:32:20 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int len;
 	char *n_str;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	len = strlen(s1) + strlen(s2) + 1;
 	n_str = (char *)malloc(len * sizeof(char));
 	if (n_str == NULL)

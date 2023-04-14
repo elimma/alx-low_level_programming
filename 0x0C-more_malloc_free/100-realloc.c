ddd91c41149e9510f229c0a4ca73ff15caa82b67
Kourdan Oumaima <ishikawaa83@gmail.com>
2023-04-13 17:19:55 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,28 +11,36 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i;
+	char *p;
+	char *q;
 	void *new_ptr;
 
 	if (new_size == 0)
 	{
 		free(ptr);
-		return (NULL);
 	}
-	if (ptr == NULL)
+	else if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		new_ptr = malloc(new_size);
 	}
-	if (old_size == new_size)
+	else if (new_size <= old_size)
 	{
-		return (ptr);
+		new_ptr = ptr;
 	}
+	else
+	{
 	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
+	if (new_ptr != NULL)
 	{
-		return (NULL);
-
+		p = (char *) ptr;
+		q = (char *) new_ptr;
+		for (i = 0; i < old_size; i++)
+		{
+			q[i] = p[i];
 	}
-	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
 	free(ptr);
+	}
+	}
 	return (new_ptr);
 }

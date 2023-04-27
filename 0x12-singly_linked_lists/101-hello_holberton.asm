36af9e0dffa23ca19f0a8252555f9c9467abb89d
elimma <ishikawaa83@gmail.com>
2023-04-27 06:07:06 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
-section .data
-    msg db 'Hello, Holberton',0
-section .text
-    global main
-    extern printf
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
 main:
-    push rbp
-    mov rbp, rsp
-    lea rdi, [msg]
-    xor eax, eax
-    call printf
-    pop rbp
-    ret
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret

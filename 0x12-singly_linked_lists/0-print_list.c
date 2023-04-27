2893efa7795331990c2c06d7fad097bbd28591d9
elimma <ishikawaa83@gmail.com>
2023-04-27 05:11:45 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
+	const list_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", current->len, current->str);
-
-        count++;
-        current = current->next;
-    }
-
-    return count;
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len, current->str);
+		count++;
+		current = current->next;
+	}
+	return (count);
 }

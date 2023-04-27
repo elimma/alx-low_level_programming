dfcfe9e50fa653592a7fb9a240f54301e57477fc
elimma <ishikawaa83@gmail.com>
2023-04-27 05:18:23 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
+	const list_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
-
-    return count;
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }

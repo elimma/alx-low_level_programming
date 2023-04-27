914c97b07fcb111b683ad569317fe8f93994c384
Kourdan Oumaima <82263436+elimma@users.noreply.github.com>
2023-04-27 12:15:08 +0000

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+    const list_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}

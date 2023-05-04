77be030fa040367574986759e1cbaf91af24abe3
elimma <ishikawaa83@gmail.com>
2023-05-04 09:57:41 -0700

    get endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	short int num = 0x01;
+	char *ptr = (char *)&num;
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
